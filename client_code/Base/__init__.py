0276248dcbb5559632aaefc122e98a83620ea5ff
varvarakrauchanka@gmail.com <varvarakrauchanka@gmail.com>
2022-06-02 12:34:38 +0000

    Created Base; Edited settings; Deleted Form1

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -1,16 +1,21 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import BaseTemplate
 from anvil import *
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
-
+class Base(BaseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+  
 
     # Any code you write here will run when the form opens.
+
+  def go_to_login_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.login_with_form()
+
 
 
