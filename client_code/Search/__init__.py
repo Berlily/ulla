90f4c44de192f310dbb7559ee634ec5e3e4f549f
varvarakrauchanka@gmail.com <varvarakrauchanka@gmail.com>
2022-06-02 22:22:52 +0000

    Created Result; Edited settings and Search; Deleted Results

--- client_code/Search/__init__.py
+++ client_code/Search/__init__.py
@@ -5,12 +5,13 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Result import Result
 
 cities = [
-  (spec['name'], spec) for spec in app_tables.specialisation.search()
+  (city['name'], city) for city in app_tables.city.search()
 ]
 
-specialisations= [
+specialisations = [
   (spec['name'], spec) for spec in app_tables.specialisation.search()
 ]
 
@@ -28,6 +29,25 @@ class Search(SearchTemplate):
     # Any code you write here will run when the form opens.
     self.specialisation_drop_down.items = specialisations
     
+    # Create DropDowns
+    self.specialisation_drop_down.items = specialisations + ["Затрудняюсь ответить"]
+    self.city_drop_down.items = cities
+
+  def city_drop_down_change(self, **event_args):
+    """This method is called when an item is selected"""
+    if self.city_drop_down.selected_value == "Минск":
+      /////
+      
+
+  def button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # go to Re
+    self.column_panel.clear()
+    self.column_panel.add_component(Result())
+
+  
+  
+
 
 
 
