57400004be6deae7dceee9564d464502e8156f91
varvarakrauchanka@gmail.com <varvarakrauchanka@gmail.com>
2022-06-03 06:36:30 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,7 +3,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-from ..Search import Search
+#from ..Search import Search
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -12,12 +12,16 @@ from ..Search import Search
 # them with @anvil.server.callable.
 
 @anvil.server.callable
-def get_therapists( city, spec, max_rate ):
+def get_therapists():
   # Get a list of therapists from the Data Table
-  city_col = city
-  specialisation_col = spec
-  max_rate_col = max_rate
   return app_tables.therapists.search()
 
+# @anvil.server.callable
+# def get_therapists( city, spec, max_rate ):
+#   # Get a list of therapists from the Data Table
+#   city_col = city
+#   specialisation_col = spec
+#   max_rate_col = max_rate
+#   return app_tables.therapists.search()
 
 
