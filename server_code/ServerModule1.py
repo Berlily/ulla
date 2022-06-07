89bd75f310dcb15faaa8d061ba88f636a9e484d7
varvarakrauchanka@gmail.com <varvarakrauchanka@gmail.com>
2022-06-07 06:08:46 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -37,9 +37,11 @@ def get_therapists( max_rate, city_var: str, spec: str ) :
     
   else if max_rate != "не имеет значения" and spec == "Затрудняюсь ответить" :
     result_view = app_tables.therapists.search(rate_in_byn = q.less_than_or_equal_to(max_rate), city= city_var)
-    
-    
+  
+  else if max_rate == "не имеет значения" and spec == "Затрудняюсь ответить" :
+    result_view = app_tables.therapists.search(city= city_var)
+  else:
+    result_view = app_tables.therapists.search(rate_in_byn = q.less_than_or_equal_to(max_rate), city= city_var, specialisation = spec)
   return result_view
 
 
-result_view = app_tables.therapists.search(rate_in_byn = q.less_than_or_equal_to(max_rate), city= city_var)
